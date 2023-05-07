b26d1c8b737f71feaa26357ce0a570874a81f669
leo03164 <leo03164@gmail.com>
2023-05-07 22:15:35 +0800

    update

--- routers/upload.js
+++ routers/upload.js
@@ -22,24 +22,34 @@ router.post('/upload', upload.single('image'), async (req, res) => {
     }
 });
 
+// 使用 Express 路由處理 POST 請求，並將上傳的單個圖片作為 'image' 的名稱參數
 router.post('/v2upload', upload.single('image'), async (req, res) => {
     try {
+        // 從 Tesseract 包中創建一個 worker 對象
         const { createWorker } = Tesseract;
         const worker = await createWorker();
+        // 加載 Tesseract 語言庫
         await worker.loadLanguage('eng');
+        // 初始化語言庫
         await worker.initialize('eng');
         
+        // 設置 Tesseract 的參數
         await worker.setParameters({
             tessedit_char_whitelist: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
           })
         
+        // 從上傳的圖片中讀取圖像數據
         const image = req.file.buffer;
 
+        // 使用 Tesseract 進行圖像識別
         const {data} = await worker.recognize(image);
+        // 將識別結果發送回客戶端
         return res.send(data.text);
     } catch (error) {
+        // 如果發生錯誤，則將錯誤消息發送回客戶端
         return res.send(error);
     }
 });
+
 
 module.exports = router;
